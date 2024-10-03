d7acef66ff0ac89055e50c10f8fcee7783720d0a
dcampas <davidcampas30@gmail.com>
2024-10-03 13:05:46 +0200

    NEW

--- Libft/ft_memcpy.c
+++ Libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: dcampas- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 12:13:50 by dcampas-          #+#    #+#             */
-/*   Updated: 2024/10/03 12:53:11 by dcampas-         ###   ########.fr       */
+/*   Updated: 2024/10/03 13:02:51 by dcampas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t	i;
-	
+
 	if (!dest && !src)
 		return (NULL);
 	i = 0;
--- Libft/ft_strncmp.c
+++ Libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: dcampas- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 15:26:23 by dcampas-          #+#    #+#             */
-/*   Updated: 2024/10/03 12:53:58 by dcampas-         ###   ########.fr       */
+/*   Updated: 2024/10/03 13:05:08 by dcampas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	if (!n)
+	if (n == 0)
 		return (0);
 	while ((s1[i] || s2[i]) && (s1[i] == s2[i]) && (i < n - 1))
 	{
 		i++;
 	}
-	return (s1[i] - s2[i]);
+	return (unsigned char)s1[i] - (unsigned char)s2[i]);
 }
 /*
 int main()
